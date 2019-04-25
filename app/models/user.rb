ce000993222e981486a40d300d1c023e7962539f
github username <github email address>
2019-04-25 17:54:57 -0400

    user model json methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,26 @@
 class User < ApplicationRecord
-    has_secure_password
-    validates :username, uniqueness: { case_sensitive: false }
     has_many :favorites
     has_many :cars, through: :favorites
+    has_secure_password
+    validates :username, uniqueness: { case_sensitive: false }, presence: true
+    validates :password, presence: true
+
+    def user_json
+        {
+            id: self.id,
+            username: self.username
+            # cars: self.cars.car_json
+            # high_score: self.high_score,
+        }
+    end
+
+    # CHANGE TO FAVORITES
+    # def high_score
+        # self.scores.collect(&:points).max
+        # self.scores.collect { |u| u.points }
+    # end
+
+    def self.user_json
+        User.all.collect { |u| u.user_json }
+    end
 end
