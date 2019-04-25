a62ecc814194cae78f53ed8e25d55b556cb0f0d0
github username <github email address>
2019-04-25 17:45:10 -0400

    car model json methods

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,4 +1,19 @@
 class Car < ApplicationRecord
     has_many :favorites
     has_many :users, through: :favorites
+
+    def car_json
+        {
+            id: self.id,
+            model: self.model,
+            make: self.make,
+            vin: self.vin,
+            # scores: self.scores.where("user_id=?", self.user_id).score_json
+            # scores: self.scores.where("user_id=?", self.user_id).collect(&:points)
+        }
+    end
+
+    def self.car_json
+        Car.all.collect { |u| u.car_json }
+    end
 end
