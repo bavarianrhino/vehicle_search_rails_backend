c8a3d5c3453c3293de96288880ff1e614004110e
github username <github email address>
2019-04-25 17:50:21 -0400

    favorite model json methods

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,21 @@
 class Favorite < ApplicationRecord
     belongs_to :user
     belongs_to :car
+
+    def favorite_json
+        {
+            username: self.user.username,
+            user_id: self.user_id,
+            car: self.car.make,
+            car: self.car.model,
+            car: self.car.year,
+            car: self.car.vin,
+            car_id: self.car_id
+        }
+    end
+
+    def self.score_json
+        # Score.all.collect { |s| s.score_json }
+        Favorite.all.collect(&:favorite_json)
+    end
 end
