eb8531c78f7e1d27e183cb87b3789259ba028439
github username <github email address>
2019-04-28 05:03:33 -0400

    indented and nested login under user route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-    resources :users
     resources :cars
     resources :favorites
-    post '/login', to: 'auth#login', as: :login
-    get '/profile', to: 'users#profile', as: :profile
+    resources :users
+        post '/login', to: 'auth#login', as: :login
+        get '/profile', to: 'users#profile', as: :profile
 end
